b3a91e47ff93ad71836c43721a4e77fe0e785b8d
Stefany Maria <sthefan1morais@gmail.com>
2026-06-01 19:46:23 -0300

    feat:Criando Repositorys

---
+++ src/main/java/com/watchly/Watchly/Dto/UsuarioResponseDTO.java
@@ -0,0 +1,4 @@
+package com.watchly.Watchly.Dto;
+
+public class UsuarioResponseDTO {
+}
---
+++ src/main/java/com/watchly/Watchly/repository/SerieRepository.java
@@ -0,0 +1,7 @@
+package com.watchly.Watchly.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SerieRepository {
+}
